1c9c80443fe683d86b449a1766e6f55bfa4337e3
Elise <74066962+elisemgun@users.noreply.github.com>
2023-06-28 17:11:01 -0700

    Added requirements.txt

--- interfaceReader.py
+++ interfaceReader.py
@@ -1,21 +1,30 @@
-from flask import Flask, request, render_template
 import openai
+import os
 import PyPDF2
+from flask import Flask, request, render_template
+from pathlib import Path
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
 # Set up your OpenAI API credentials
-openai.api_key = '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'
+openai.api_key = os.environ['OPENAI_KEY']
+pdf_text = ""  # Global variable to store the text extracted from the PDF
 
 
-# Function to extract text from PDF using PyPDF2
+# Read the PDF file and extract text at the start of the application
 def extract_text_from_pdf(file_path):
-    with open(file_path, 'rb') as file:
+    pdf_file_path = 'FAQ.pdf'
+    with open(pdf_file_path, 'rb') as file:
         pdf_reader = PyPDF2.PdfReader(file)
-        text = ''
+        pdf_text = ''
         for page in pdf_reader.pages:
-            text += page.extract_text()
-        return text
+            pdf_text += page.extract_text()
+        return pdf_text
 
 
 # Function to generate responses using OpenAI API
@@ -23,7 +32,7 @@ def generate_responses(prompt):
     response = openai.Completion.create(
         engine='text-davinci-003',
         prompt=prompt,
-        max_tokens=100,
+        max_tokens=1000,
         n=1,
         stop=None,
         temperature=0.1
@@ -31,31 +40,28 @@ def generate_responses(prompt):
     return response.choices[0].text.strip()
 
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    if request.method == 'POST':
+        question = request.form['question']
 
+        # Generate responses based on the extracted text and user question
+        prompt = "Consider the following text from the PDF" \
+                 f"{pdf_text}\n\n" \
+                 f"The user question is: \"{question}\"" \
+                 "\nIf the question is answered in this FAQ database, provide the answer in natural language. " \
+                 "If the question is not answered in this FAQ database, reply that you are not" \
+                 "sure and will contact an agent."
 
-@app.route('/getresponse', methods=['POST'])
-def get_response():
-    question = request.form['question']
+        response = generate_responses(prompt)
 
-    # Read the PDF file and extract text
-    pdf_file_path = 'FAQ.pdf'
-    pdf_text = extract_text_from_pdf(pdf_file_path)
-
-    # Generate responses based on the extracted text and user question
-    prompt = "Consider the following text from the PDF as the FAQ database:\n" \
-             f"{pdf_text}\n\n" \
-             f"The user question is: \"{question}\"" \
-             "\nIf the question is answered in this FAQ database, provide the answer. " \
-             "If the question is not answered in this FAQ database, reply that you are not" \
-             "sure and will contact an agent."
-
-    response = generate_responses(prompt)
-    print(response)
-    return render_template('response.html', response=response, question=question)
+        return render_template('index.html', question=question, response=response)
+    else:
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
+    pdf_file_path = 'FAQ.pdf'
+    pdf_text = extract_text_from_pdf(pdf_file_path)
     app.run(debug=True)
--- terminalReader.py
+++ terminalReader.py
@@ -1,9 +1,15 @@
 import openai
 import PyPDF2
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
 # Set up your OpenAI API credentials
-openai.api_key = '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'
-
+openai.api_key = os.environ['OPENAI_KEY']
+pdf_text = ""  # Global variable to store the text extracted from the PDF
 
 # Function to extract text from PDF using PyPDF2
 def extract_text_from_pdf(file_path):
@@ -31,15 +37,19 @@ def generate_responses(prompt):
 # Main program
 def main():
     # Read the PDF file and extract text
-    pdf_file_path = 'FAQ.pdf'
+    pdf_file_path = 'FAQ-complete.pdf'
     pdf_text = extract_text_from_pdf(pdf_file_path)
 
     question = input("Enter your question: ")
 
     # Generate responses based on the extracted text and user question
-    prompt = "Read the following text from the PDF. " \
-             "If the question is not answered in this FAQ database rely that you are not sure and will contact an agent." \
-             "\n" + pdf_text + question
+    prompt = "This is a FAQ database in table format for insurance with questions and answers. Consider the following " \
+             "text from the PDF" \
+             f"{pdf_text}\n\n" \
+             f"The user question is: \"{question}\"" \
+             "If the question is answered in this FAQ database, provide the answer in natural language. " \
+             "If the question is not answered in this FAQ database, reply that you are not" \
+             "sure and will contact an agent."
 
     response = generate_responses(prompt)
     print("Generated response:")
